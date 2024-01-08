5602ec096364ece4cd0c085def78b93dbc81ca9f
MoreCodes <arjunaraneta@gmail.com>
2024-01-08 12:36:03 +0800

    React Project Folder

--- 01-birthday-reminder/src/data.js
+++ 01-birthday-reminder/src/data.js
@@ -4,7 +4,7 @@ export default [
     name: 'Olivia Bennett',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 2,
