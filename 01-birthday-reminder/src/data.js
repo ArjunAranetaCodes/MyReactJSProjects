d3865c3e440721ee045b06e7a478664b59ba19ae
MoreCodes <arjunaraneta@gmail.com>
2024-01-08 12:37:20 +0800

    React Project Folder

--- 01-birthday-reminder/src/data.js
+++ 01-birthday-reminder/src/data.js
@@ -25,7 +25,7 @@ export default [
     name: 'Marcus Mitchell',
     age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 5,
